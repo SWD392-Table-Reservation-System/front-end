1d74adb1f0d7cedc486476b635e4139169d6ac29
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-06-17 14:33:36 +0700

    Thong - Update icon, add login page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import logo from './assets/swd-transformed_notext.svg';
+
 import './App.scss';
 
 import { Button } from 'primereact/button';
+import Login from './components/login/login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <BrowserRouter>
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      </header>
-      <Button label="Submit" icon="pi pi-check" />
+      </header> */}
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
+        <Button label="Submit" icon="pi pi-check" />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,14 +1,77 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './login.module.scss';
+import React, { useRef } from "react";
+import './login.scss';
+import { Image } from 'primereact/image';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { useFormik } from 'formik';
+import { Toast } from 'primereact/toast';
+import { Link, useNavigate } from "react-router-dom";
 
-const Login = () => (
-  <div className={styles.Login}>
-    Login Component
-  </div>
-);
 
-Login.propTypes = {};
+
+
+const Login = () => {
+
+    const toast = useRef(null);
+
+    const navigate = useNavigate();
+
+    const show = () => {
+        toast.current.show({ severity: 'success', summary: 'Form Submitted', detail: formik.values.item.toString() });
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            password: ""
+        },
+        validate: (data) => {
+            let errors = {};
+
+            if (data.username) {
+                errors.username = 'Username is required.';
+            }
+            if (data.password) {
+                errors.password = 'Password is required.';
+            }
+
+            return errors;
+        },
+        onSubmit: (data) => {
+            if (!formik.validate) {
+                console.log('Login sucessful');
+                navigate('/home')
+            }
+            data.username && data.password && show();
+            console.log("hello");
+            formik.resetForm();
+        }
+    });
+
+    const isFormFieldInvalid = (name) => !!(formik.touched[name] && formik.errors[name]);
+
+    const getFormErrorMessage = (name) => {
+        return isFormFieldInvalid(name) ? <small className="p-error">{formik.errors[name]}</small> : <small className="p-error">&nbsp;</small>;
+    };
+
+    return (
+        <div className='loginComponentContainer'>
+            <Toast ref={toast} />
+            <h2 className="loginComponentTitle">Welcome to Restaurant Reservation System website</h2>
+            {/* <Image src="swd-transformed_notext.svg" alt="Image" width="250" /> */}
+            <form onSubmit={formik.handleSubmit} className="loginComponentForm">
+                <InputText id="username" type="text" className="p-inputtext-lg" placeholder="Email or Username" required="true" />
+                <InputText id="password" type="password" className="p-inputtext-lg" placeholder="Password" required="true" />
+                <Button label="Login" type="submit" />
+
+            </form>
+
+
+        </div>
+    )
+
+};
+
 
 Login.defaultProps = {};
 
