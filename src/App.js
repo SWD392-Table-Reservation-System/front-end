3d772c631f1362608a654e1450ba8c8e47d3ad88
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-06-20 14:59:35 +0700

    Anh - Add 'onboard' components and create home.js for Home page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,25 @@ import { Link } from "react-router-dom";
 
 //import pages
 import Login from "./components/login/login";
+import Home from "./components/onboard/home";
 
 function App() {
   return (
     <div className="App">
-     <Login/>
+      <BrowserRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to={"/"}>Home</Link> 
+                <Link to={"/signin"}>Sign in</Link> 
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route exact path="/" element={ <Home /> }/>
+            <Route exact path="/signin" element={ <Login /> }/>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
