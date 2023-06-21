e52a8dca2f4cfeb5fe9c67045a33f16fac686869
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-06-21 11:28:47 +0700

    Thong - complete make order fetch api

--- src/components/makeOrder/makeOrder.js
+++ src/components/makeOrder/makeOrder.js
@@ -3,37 +3,158 @@ import PropTypes from "prop-types";
 import styles from "./makeOrder.module.scss";
 
 const MakeOrder = () => {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [time, setTime] = useState("09:00 AM");
+  const [date, setDate] = useState("");
+  const [customerFullName, setCustomerFullName] = useState("");
+  const [customerEmail, setCustomerEmail] = useState("");
+  const [customerPhoneNumber, setCustomerPhoneNumber] = useState("");
+  const [note, setNote] = useState("");
+  // Format the time value to remove AM/PM and leading zero
+  const formattedTime = time.replace(/[^\d:]/g, "");
+  const adjustedDate = date;
+  const dateTimeBooking = `${adjustedDate}T${formattedTime}:00`;
+  const customerQuantity = number1 + number2;
 
-  const [number, setNumber] = useState(0);
-
-  const incrementNumber = () => {
-    setNumber((prevNumber) => prevNumber + 1);
-  };
-
-  const decrementNumber = () => {
-    if (number > 0) {
-      setNumber((prevNumber) => prevNumber - 1);
+  const increment = (inputId) => {
+    if (inputId === "number1") {
+      setNumber1(number1 + 1);
+    } else if (inputId === "number2") {
+      setNumber2(number2 + 1);
     }
   };
-  
+
+  const decrement = (inputId) => {
+    if (inputId === "number1" && number1 > 0) {
+      setNumber1(number1 - 1);
+    } else if (inputId === "number2" && number2 > 0) {
+      setNumber2(number2 - 1);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const apiUrl = process.env.REACT_APP_API_URL;
+
+    // Construct the request body
+    const requestBody = JSON.stringify({
+      dateTimeBooking: dateTimeBooking,
+      customerQuantity: customerQuantity,
+      note: note,
+      customerFullName: customerFullName,
+      customerEmail: customerEmail,
+      customerPhoneNumber: customerPhoneNumber,
+    });
+    console.log(requestBody);
+
+    // Perform the API request
+    fetch(`${apiUrl}/api/Reservations`, {
+      method: "POST", headers: {
+        "Content-Type": "application/json"
+      },
+      body: requestBody,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(
+            `API request failed: ${response.status} ${response.statusText}`
+          );
+        }
+      })
+      .then((data) => {
+        console.log("API response:", data);
+      })
+      .catch((error) => {
+        console.error("API request error:", error);
+      });
+  };
+
+
   return (
     <div className={styles.MakeOrder}>
       <div className={styles["container"]}>
         <div className={styles["thumbnail"]}>
-          <img className={styles["img"]} src="orderPageImg.png" alt></img>
+          <img className={styles["img"]} src="orderPageImg.png" alt="" />
           <h1 className={styles["res-name"]}>RESTAURANT NAME</h1>
           <p className={styles["res-address"]}>Restaurant address</p>
         </div>
 
         <div className={styles["order-menu"]}>
-          <div className={styles["people-quantity"]}>
-            <div className={styles["adult-quantity"]}>
-              <h3>No. of Adults</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group input-group-quantity">
+              <input
+                type="number"
+                id="number1"
+                value={number1}
+                onChange={(e) => setNumber1(parseInt(e.target.value))}
+              />
+              <button type="button" onClick={() => decrement('number1')}>-</button>
+              <button type="button" onClick={() => increment('number1')}>+</button>
+
+              <input
+                type="number"
+                id="number2"
+                value={number2}
+                onChange={(e) => setNumber2(parseInt(e.target.value))}
+              />
+              <button type="button" onClick={() => decrement('number2')}>-</button>
+              <button type="button" onClick={() => increment('number2')}>+</button>
             </div>
-            <div className={styles["children-quantity"]}>
-              <h3>No. of Children</h3>
+
+
+
+            <div className="input-group input-group-time">
+              <select
+                id="time"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}>
+                <option value="9:00 AM">09:00 AM</option>
+                <option value="10:00 AM">10:00 AM</option>
+                <option value="11:00 AM">11:00 AM</option>
+              </select>
             </div>
-          </div>
+
+            <div className="input-group input-group-date">
+              <input type="date" id="date" value={date}
+                onChange={(e) => setDate(e.target.value)} />
+            </div>
+
+            <div className="input-group input-group-personal">
+              <input
+                type="text"
+                id="customerFullName"
+                value={customerFullName}
+                onChange={(e) => setCustomerFullName(e.target.value)}
+                placeholder="Full Name"
+              />
+              <input
+                type="email"
+                id="customerEmail"
+                value={customerEmail}
+                onChange={(e) => setCustomerEmail(e.target.value)}
+                placeholder="Email"
+              />
+              <input
+                type="tel"
+                id="customerPhoneNumber"
+                value={customerPhoneNumber}
+                onChange={(e) => setCustomerPhoneNumber(e.target.value)}
+                placeholder="Phone Number"
+              />
+              <textarea
+                id="note"
+                value={note}
+                onChange={(e) => setNote(e.target.value)}
+                placeholder="Note"
+              ></textarea>
+            </div>
+
+
+            <button type="submit">Submit</button>
+          </form>
         </div>
       </div>
     </div>
