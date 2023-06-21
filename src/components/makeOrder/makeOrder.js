beb562e851dda3f002890b320f29135b17fa669a
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-06-21 09:54:05 +0700

    Anh - Update Make Order Page

--- src/components/makeOrder/makeOrder.js
+++ src/components/makeOrder/makeOrder.js
@@ -2,34 +2,43 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./makeOrder.module.scss";
 
+const MakeOrder = () => {
 
+  const [number, setNumber] = useState(0);
 
+  const incrementNumber = () => {
+    setNumber((prevNumber) => prevNumber + 1);
+  };
 
-const MakeOrder = () => (
-  <div className={styles.MakeOrder}>
-    <div className={styles["container"]}>
-      
-      <div className={styles["thumbnail"]}>
-        <img className={styles["img"]} src="orderPageImg.png" alt></img>
-        <h1 className={styles["res-name"]}>
-          RESTAURANT NAME
-        </h1>
-        <p className={styles["res-address"]}>Restaurant address</p>
-      </div>
+  const decrementNumber = () => {
+    if (number > 0) {
+      setNumber((prevNumber) => prevNumber - 1);
+    }
+  };
+  
+  return (
+    <div className={styles.MakeOrder}>
+      <div className={styles["container"]}>
+        <div className={styles["thumbnail"]}>
+          <img className={styles["img"]} src="orderPageImg.png" alt></img>
+          <h1 className={styles["res-name"]}>RESTAURANT NAME</h1>
+          <p className={styles["res-address"]}>Restaurant address</p>
+        </div>
 
-      <div className={styles["order-menu"]}>
-        <div className={styles["people-quantity"]}>
-          <div className={styles["adult-quantity"]}>
-            <h3>No. of Adults</h3>
-          </div>
-          <div className={styles["children-quantity"]}>
-            <h3>No. of Children</h3>
+        <div className={styles["order-menu"]}>
+          <div className={styles["people-quantity"]}>
+            <div className={styles["adult-quantity"]}>
+              <h3>No. of Adults</h3>
+            </div>
+            <div className={styles["children-quantity"]}>
+              <h3>No. of Children</h3>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 MakeOrder.propTypes = {};
 
