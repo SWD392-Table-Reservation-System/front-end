bfdb5778079db5435dc0b84d3b446a77fde87423
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-07-11 01:08:03 +0700

    Thong - update reservationList

--- src/components/tableMana/reservationList.js
+++ src/components/tableMana/reservationList.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import styles from './reservationList.scss';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const ReservationList = () => {
@@ -32,10 +36,25 @@ const ReservationList = () => {
         fetchReservationList();
     }, []);
 
+    const footer = (
+        <div className="flex flex-wrap justify-content-end gap-2">
+            <Button label="Delete" icon="pi pi-times" className="p-button-outlined p-button-secondary" />
+        </div>
+    );
+
     return (
         <div className={styles.ReservationList}>
             {reservations.map((reservation, index) => (
-                <div key={index}>{reservation.customerFullName}</div>
+                <Card key={index} title='Detail' footer={footer} style={{ margin: '1rem' }}>
+                    <p>Id: {reservation.id}</p>
+                    <p>Creation Date: {reservation.creationDate}</p>
+                    <p>Booking Date: {reservation.dateTimeBooking}</p>
+                    <p>Customer Quantity: {reservation.customerQuantity}</p>
+                    <p>Note: {reservation.note}</p>
+                    <p>Customer Name: {reservation.customerFullName}</p>
+                    <p>Customer Email: {reservation.customerEmail}</p>
+                    <p>Customer Phone Number: {reservation.customerPhoneNumber}</p>
+                </Card>
             ))}
         </div>
     );
