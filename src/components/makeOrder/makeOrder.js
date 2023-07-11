30f1f53d531c2e39c9d263b00e28af15440844b7
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-07-11 08:44:38 +0700

    Anh - Fix Make Order Page UI

--- src/components/makeOrder/makeOrder.js
+++ src/components/makeOrder/makeOrder.js
@@ -1,8 +1,13 @@
 import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import styles from "./makeOrder.module.scss";
-import { Toast } from 'primereact/toast';
-import { useNavigate } from 'react-router-dom';
+import { Toast } from "primereact/toast";
+import { useNavigate } from "react-router-dom";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Dropdown } from "primereact/dropdown";
+import { Calendar } from "primereact/calendar";
+import { Link } from "react-router-dom";
 
 const MakeOrder = () => {
   const navigate = useNavigate();
@@ -18,22 +23,21 @@ const MakeOrder = () => {
   const [note, setNote] = useState("");
   const toast = useRef(null);
   const [timeArray, setTimeArray] = useState([]);
-  // Format the time value to remove AM/PM and leading zero
+
   const formattedTime = time.replace(/[^\d:]/g, "");
   const adjustedDate = date;
   const dateTimeBooking = `${adjustedDate}T${formattedTime}`;
   const customerQuantity = number1 + number2;
-  const getTime = () => {
 
+  const getTime = () => {
     fetch(`${apiUrl}/api/Test/time-list`, {
       method: "GET",
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     })
       .then((response) => {
         if (response.ok) {
-          console.log(response);
           return response.json();
         } else {
           throw new Error(
@@ -42,43 +46,39 @@ const MakeOrder = () => {
         }
       })
       .then((data) => {
-        console.log("getTime API response data:", data);
         setTimeArray(data);
       })
       .catch((error) => {
         console.error("getTime API request error:", error);
       });
-  }
+  };
 
   useEffect(() => {
-    getTime()
-  }, [])
-
-
+    getTime();
+  }, []);
 
   const show = (severity, summary, detail) => {
-    toast.current.show({ severity: severity, summary: summary, detail: detail });
+    toast.current.show({ severity, summary, detail });
   };
 
   const increment = (inputId) => {
     if (inputId === "number1") {
-      setNumber1(number1 + 1);
+      setNumber1((prevNumber) => prevNumber + 1);
     } else if (inputId === "number2") {
-      setNumber2(number2 + 1);
+      setNumber2((prevNumber) => prevNumber + 1);
     }
   };
 
   const decrement = (inputId) => {
     if (inputId === "number1" && number1 > 0) {
-      setNumber1(number1 - 1);
+      setNumber1((prevNumber) => prevNumber - 1);
     } else if (inputId === "number2" && number2 > 0) {
-      setNumber2(number2 - 1);
+      setNumber2((prevNumber) => prevNumber - 1);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Construct the request body
     const requestBody = JSON.stringify({
       dateTimeBooking: dateTimeBooking,
       customerQuantity: customerQuantity,
@@ -87,139 +87,220 @@ const MakeOrder = () => {
       customerEmail: customerEmail,
       customerPhoneNumber: customerPhoneNumber,
     });
-    console.log(requestBody);
 
-    // Perform the API request
     fetch(`${apiUrl}/api/Reservations`, {
-      method: "POST", headers: {
-        "Content-Type": "application/json"
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
       body: requestBody,
     })
       .then((response) => {
         if (response.ok) {
-          console.log(response);
           return response.json();
         } else {
-          show('error', 'Ordered fail', `${response.statusText}`)
+          show("error", "Order failed", `${response.statusText}`);
           throw new Error(
             `API request failed: ${response.status} ${response.statusText}`
           );
         }
       })
       .then((data) => {
-        console.log("API response:", data);
-        show('success', `Hello ${data.data.customerFullName}, you ordered successfully!`, 'You are directing to Order Detail')
+        show(
+          "success",
+          `Hello ${data.data.customerFullName}, your order was successful!`,
+          "You are being directed to the Order Detail page."
+        );
         setTimeout(() => {
-          navigate('/order/success')
+          navigate("/order/success");
         }, 3000);
-
       })
       .catch((error) => {
         console.error("API request error:", error);
-        show('error', 'Ordered fail', `${error}`)
+        show("error", "Order failed", `${error}`);
       });
   };
-
-
 
   return (
     <div className={styles.MakeOrder}>
       <Toast ref={toast} />
-      <div className={styles["container"]}>
-        <div className={styles["thumbnail"]}>
-          <img className={styles["img"]} src="orderPageImg.png" alt="" />
+      <div className={styles.container}>
+        <div className={styles.thumbnail}>
+          <img className={styles.img} src="orderPageImg.png" alt="" />
           <h1 className={styles["res-name"]}>RESTAURANT NAME</h1>
           <p className={styles["res-address"]}>Restaurant address</p>
         </div>
 
-        <div className={styles["order-menu"]}>
-          <form onSubmit={handleSubmit}>
-            <div className="input-group input-group-quantity">
-              <div>
-                <label for="number1">Number of Adult: </label>
-                <input
-                  type="number"
-                  id="number1"
-                  value={number1}
-                  onChange={(e) => setNumber1(parseInt(e.target.value))}
-                  required="true"
-                />
-                <button type="button" onClick={() => decrement('number1')}>-</button>
-                <button type="button" onClick={() => increment('number1')}>+</button>
+        <div className={styles.orderMenu}>
+          <form className={styles.inputsForm} onSubmit={handleSubmit}>
+            <div
+              id={styles.inputAdultQuantity}
+              className="input-group input-group-quantity"
+            >
+              <div className={styles.inputAdults}>
+                <span className={styles.adultQuantityLabel}>
+                  <label htmlFor="number1">Number of Adults: </label>
+                </span>
+                <span className={styles.adultQuantityChangeButton}>
+                  <button
+                    className={styles.decrementAdult}
+                    type="button"
+                    onClick={() => decrement("number1")}
+                  >
+                    -
+                  </button>
+                  <input
+                    className={styles.displayAdultQuantity}
+                    id="number1"
+                    type="number"
+                    value={number1}
+                    onChange={(e) => setNumber1(parseInt(e.target.value))}
+                    required
+                  />
+                  <button
+                    className={styles.incrementAdult}
+                    type="button"
+                    onClick={() => increment("number1")}
+                  >
+                    +
+                  </button>
+                </span>
               </div>
 
-              <div>
-                <label for="number2">Number of Child(ren): </label>
-                <input
-                  type="number"
-                  id="number2"
-                  value={number2}
-                  onChange={(e) => setNumber2(parseInt(e.target.value))}
-                  required="true"
-                />
-                <button type="button" onClick={() => decrement('number2')}>-</button>
-                <button type="button" onClick={() => increment('number2')}>+</button>
+              <div className={styles.inputChildrenQuantity}>
+                <div className={styles.inputChildren}>
+                  <span className={styles.childrenQuantityLabel}>
+                    <label htmlFor="number2">Number of Children: </label>
+                  </span>
+                  <span className={styles.childrenQuantityChangeButton}>
+                    <button
+                      className={styles.decrementChildren}
+                      type="button"
+                      onClick={() => decrement("number2")}
+                    >
+                      -
+                    </button>
+                    <input
+                      className={styles.displayChildrenQuantity}
+                      id="number2"
+                      type="number"
+                      value={number2}
+                      onChange={(e) => setNumber2(parseInt(e.target.value))}
+                      required
+                    />
+                    <button
+                      className={styles.incrementChildren}
+                      type="button"
+                      onClick={() => increment("number2")}
+                    >
+                      +
+                    </button>
+                  </span>
+                </div>
               </div>
-
             </div>
 
-
-
-            <div className="input-group input-group-time">
-              <label for="time">Time: </label>
-              <select
+            <div
+              id={styles.inputGroupTime}
+              className="input-group input-group-time"
+            >
+              <span className={styles.timeLabel}>
+                <label htmlFor="time">Time: </label>
+              </span>
+              <Dropdown
+                className={styles.dropdown}
                 id="time"
                 value={time}
-                onChange={(e) => setTime(e.target.value)}>
-                {
-                  timeArray.map(time => <option value={time}>{time.slice(0, -3)}</option>)
-                }
-              </select>
-            </div>
-
-            <div className="input-group input-group-date">
-              <label for="date">Date: </label>
-              <input type="date" id="date" value={date}
-                onChange={(e) => setDate(e.target.value)} required="true"
+                options={timeArray}
+                onChange={(e) => setTime(e.value)}
               />
             </div>
 
-            <div className="input-group input-group-personal">
-              <input
-                type="text"
-                id="customerFullName"
-                value={customerFullName}
-                onChange={(e) => setCustomerFullName(e.target.value)}
-                placeholder="Full Name"
-                required="true"
+            <div
+              id={styles.inputGroupDate}
+              className="input-group input-group-date"
+            >
+              <span className={styles.dateLabel}>
+                <label htmlFor="date">Date: </label>
+              </span>
+              <Calendar
+                id="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                showIcon
+                style={{ width: "120px" }}
+                required
               />
-              <input
-                type="email"
-                id="customerEmail"
-                value={customerEmail}
-                onChange={(e) => setCustomerEmail(e.target.value)}
-                placeholder="Email"
-                required="true"
-              />
-              <input
-                type="tel"
-                id="customerPhoneNumber"
-                value={customerPhoneNumber}
-                onChange={(e) => setCustomerPhoneNumber(e.target.value)}
-                placeholder="Phone Number"
-                required="true"
-              />
-              <textarea
-                id="note"
-                value={note}
-                onChange={(e) => setNote(e.target.value)}
-                placeholder="Note"
-              ></textarea>
             </div>
 
+            <div
+              id={styles.inputGroupPersonal}
+              className="input-group input-group-personal"
+            >
+              <label className={styles.personalInfoLabel} for="personal">
+                Personal Information:
+              </label>
 
-            <button type="submit">Submit</button>
+              <div className={styles.inputTextPersonal}>
+                <label className={styles.label} htmlFor="customerFullName">
+                  Full Name:
+                </label>
+                <input
+                  class={styles.underlineInput}
+                  type="text"
+                  id="customerFullName"
+                  value={customerFullName}
+                  onChange={(e) => setCustomerFullName(e.target.value)}
+                  placeholder="Abc"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputTextPersonal}>
+                <label className={styles.label} htmlFor="customerFullName">
+                  Email:
+                </label>
+                <input
+                  class={styles.underlineInput}
+                  id="customerEmail"
+                  type="email"
+                  value={customerEmail}
+                  onChange={(e) => setCustomerEmail(e.target.value)}
+                  placeholder="email@gmail.com"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputTextPersonal}>
+                <label className={styles.label} htmlFor="customerFullName">
+                  Phone:
+                </label>
+                <input
+                  class={styles.underlineInput}
+                  id="customerPhoneNumber"
+                  value={customerPhoneNumber}
+                  onChange={(e) => setCustomerPhoneNumber(e.target.value)}
+                  placeholder="Phone Number"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputTextPersonal}>
+                <label className={styles.label} htmlFor="customerFullName">
+                  Note:
+                </label>
+                <textarea
+                  className={styles.underlineNote}
+                  id="note"
+                  value={note}
+                  onChange={(e) => setNote(e.target.value)}
+                  placeholder="Note"
+                />
+              </div>
+            </div>
+            <Link to={"/order/success"}>
+              <Button  style={{ marginTop: "10px" }} type="submit" label="Submit" />
+            </Link>
           </form>
         </div>
       </div>
