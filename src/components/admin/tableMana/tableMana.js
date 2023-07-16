ab62c3f10ae671b0280534f734f628d993e2c848
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-07-16 20:32:26 +0700

    Anh - Update Table Detail in Table Mana UI

--- src/components/admin/tableMana/tableMana.js
+++ src/components/admin/tableMana/tableMana.js
@@ -2,10 +2,10 @@ import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./tableMana.module.scss";
 import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog"; // Import Dialog component
 import Navbar from "../../common/navbar/navbar";
-import { Toast } from 'primereact/toast';
-import axiosCustom from '../../../utils/axiosConfig'
-
+import { Toast } from "primereact/toast";
+import axiosCustom from "../../../utils/axiosConfig";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -13,6 +13,7 @@ const TableMana = () => {
   const toast = useRef(null);
   const [tableData, setTableData] = useState([{ code: "Table 01", status: 0 }]);
   const [tableDetail, setTableDetail] = useState({ code: "", status: "", seatQuantity: "" });
+  const [showTableDetails, setShowTableDetails] = useState(false); // Track the visibility of the modal
 
   const getTables = () => {
     const bearerToken = localStorage.getItem("token");
@@ -43,24 +44,26 @@ const TableMana = () => {
   }, []);
 
   const getTableById = (id) => {
-    const bearerToken = localStorage.getItem('token');
-    axiosCustom.get(`${apiUrl}/api/Tables/${id}`, {
-      headers: {
-        Authorization: `Bearer ${bearerToken}`,
-      }
-    })
-      .then(response => {
+    const bearerToken = localStorage.getItem("token");
+    axiosCustom
+      .get(`${apiUrl}/api/Tables/${id}`, {
+        headers: {
+          Authorization: `Bearer ${bearerToken}`,
+        },
+      })
+      .then((response) => {
         console.log(response);
         if (response.data.success) {
           console.log(response.data.data);
           setTableDetail(response.data.data);
+          setShowTableDetails(true); // Show the modal when table details are retrieved
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-        toast.current.show({ severity: 'error', summary: 'Error', detail: '' });
+        toast.current.show({ severity: "error", summary: "Error", detail: "" });
       });
-  }
+  };
 
   const handleClick = (id, label) => {
     console.log(`Button ${label} was clicked.`);
@@ -72,7 +75,8 @@ const TableMana = () => {
   for (let i = 0; i < tableData.length; i++) {
     const row = [];
 
-    for (let j = 0; j < 6; j++) { // Limit the number of columns to 6
+    for (let j = 0; j < 6; j++) {
+      // Limit the number of columns to 6
       const index = i * 6 + j;
       if (index < tableData.length) {
         const entry = tableData[index];
@@ -96,71 +100,48 @@ const TableMana = () => {
           </td>
         );
       } else {
-        row.push(<td key={index}></td>); // Add an empty cell if there is no data for the current index
+        row.push(<td key={index}></td>);
       }
     }
 
     table.push(<tr key={i}>{row}</tr>);
   }
 
+  const onHideTableDetails = () => {
+    setShowTableDetails(false); // Hide the modal when closed
+    setTableDetail({ code: "", status: "", seatQuantity: "" }); // Reset the table details
+  };
+
   return (
     <div className={styles.TableMana}>
       <Toast ref={toast} />
       <div className={styles.displayTables}>
         <h1>Table Management</h1>
-
-        <span className={styles.annotate}>
-          <div style={{ width: 17, height: 17, background: "#CD672E", borderRadius: 9999 }} />
-          <label>Empty</label>
-          <div style={{ width: 17, height: 17, background: "#48EF45", borderRadius: 9999 }} />
-          <label>Booking</label>
-          <div style={{ width: 17, height: 17, background: "#FFF500", borderRadius: 9999 }} />
-          <label>Blocked</label>
-        </span>
-
-        <span className={styles.time}>
-          <div className={styles.date}>
-            <div className={styles.customContainer}>
-              <div className={styles.customItem}>
-                <span>DD</span>
-              </div>
-              <div className={styles.customItem}>
-                <span>MM</span>
-              </div>
-              <div className={styles.customItem}>
-                <span>YY</span>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <div className={styles.customContainer}>
-              <div className={styles.customItem}>
-                <span>HH</span>
-              </div>
-              <div className={styles.customItem}>
-                <span>MM</span>
-              </div>
-              <div className={styles.customItem}>
-                <span>AM</span>
-              </div>
-            </div>
-          </div>
-        </span>
-
+        {/* ... */}
         <table>
           <tbody>{table}</tbody>
         </table>
-
-        {tableDetail.code === "" ? "" : (
-          <div className={`${styles.tableDetails} card`}>
-            <h3>Table detail</h3>
-            <p>{tableDetail.code}</p>
-            <p>{tableDetail.status}</p>
-            <p>{tableDetail.seatQuantity}</p>
-          </div>
-        )}
-
+        <Dialog
+        visible={showTableDetails}
+        onHide={onHideTableDetails}
+        className={styles.tableDetails}
+        header="Table detail"
+        modal
+        breakpoints={{ "960px": "75vw" }}
+        style={{ width: "50vw" }}
+      >
+        <div className={styles.dialogContent}>
+          <p>
+            <strong>Code:</strong> {tableDetail.code}
+          </p>
+          <p>
+            <strong>Status:</strong> {tableDetail.status}
+          </p>
+          <p>
+            <strong>Seat Quantity:</strong> {tableDetail.seatQuantity}
+          </p>
+        </div>
+      </Dialog>
       </div>
     </div>
   );
